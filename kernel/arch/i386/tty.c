36da2a99a35f8e2a90989533a2fb605935015f82
chair1842 <207965457+chair1842@users.noreply.github.com>
2025-07-21 19:55:23 +0800

    fix newline again

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -41,18 +41,16 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 void terminal_putchar(char c) {
 	unsigned char uc = c;
 	if (c == '\n') {
-		uc = ' ';
-	}
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (c == '\n') {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
-	}
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	} else {
+		terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+				terminal_row = 0;
+		}
 	}
 }
 
